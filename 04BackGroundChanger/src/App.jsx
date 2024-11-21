aedd82d354d2e5ab09c0f35e5bd6919a1826545b
Zagajr <131444399+Zagajr@users.noreply.github.com>
2024-11-22 00:01:09 +0530

    04BGChanger

---
+++ 04BackGroundChanger/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-500 text-3xl' > A backGround changer App with React and Vite </h1>
+    </>
+  )
+}
+
+export default App
